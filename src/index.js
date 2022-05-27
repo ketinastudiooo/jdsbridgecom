42b9467af45a147f08dbb22dda31c96ca00d7b57
Rice <riceeechang@gmail.com>
2022-05-28 02:35:29 +0800

    horizontal scroll 實現

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const pizzaDotStyle = {
     secondary: "bg-main",
     normal: "bg-second"
 }
+
 const mobile_icon = document.getElementById("mobile-icon");
 const mobile_menu = document.getElementById("mobile-menu");
+// hamburgers 三條線
 const hamburgers = document.querySelectorAll("#mobile-icon .button-hamburger div");
+
 const navBar = document.querySelector("nav");
 const sections = document.querySelectorAll("section.section_item");
 const navLi = document.querySelectorAll("nav .container ul li");
+
 const compoundList = document.querySelector("#compound .slick-container .item-list");
-const pizzaBox = document.querySelector("#product-show .product-box");
+/*const pizzaBox = document.querySelector("#product-show .product-box");*/
 const pizzaNumber = document.querySelector("#product-show .now");
 const pizzaDots = document.querySelectorAll("#product-show .deco-dot li");
 
@@ -56,21 +60,30 @@ function setPizzaDot(index) {
         }
     });
 }
+/* 
+// product 數字跳動
+let now = 0;
+pizzaBox.addEventListener("scroll", (e) => {
+    let single = 0;
+    let index = 0;
 
-function setNavStatus() {
-    //navBar change color
-    if (window.scrollY > 30) {
-        navBar.classList.add("scrolled");
-        hamburgers.forEach((item) => {
-            item.style.background = "#3279ce";
+    single = (e.target.scrollWidth / 5) - 10;
+    index = Math.trunc(e.target.scrollLeft / single);
+    console.log(`total: ${e.target.scrollWidth}, scrollLeft: ${e.target.scrollLeft}, single: ${single}, index: ${index}`);
+
+    now = index + 1;
+
+    if (Number(pizzaNumber.innerHTML) !== now) {
+        pizzaNumber.innerHTML = "0" + now.toFixed();
+        pizzaDots.forEach((item) => {
+            item.setAttribute("class", "");
         });
-    } else {
-        navBar.classList.remove("scrolled");
-        hamburgers.forEach((item) => {
-            item.style.background = "#FFF";
-        });
+
+        setPizzaDot(index);
     }
-}
+});
+*/
+
 
 let navIsOpen = false;
 let mobileNavTimeLine = gsap.timeline({ paused: true });
@@ -141,15 +154,10 @@ mobile_menu.querySelectorAll("ul li a").forEach((item) => {
         if (item.innerHTML.toLowerCase() === "home") {
             return;
         }
-
-        // let target = document.getElementById(e.target.innerHTML.toLowerCase());
-        // window.scrollTo({
-        //     top: target.offsetTop - document.querySelector("nav").clientHeight,
-        //     behavior: "smooth"
-        // });
     });
 });
 
+// 這個是做視差滾動的
 gsap.to("#products .parallax-refactor .image", {
     yPercent: -80,
     scrollTrigger: {
@@ -158,52 +166,12 @@ gsap.to("#products .parallax-refactor .image", {
     }
 });
 
-let now = 0;
-pizzaBox.addEventListener("scroll", (e) => {
-    let single = 0;
-    let index = 0;
-
-    single = (e.target.scrollWidth / 5) - 10;
-    index = Math.trunc(e.target.scrollLeft / single);
-    console.log(`total: ${e.target.scrollWidth}, scrollTop: ${e.target.scrollLeft}, single: ${single}, index: ${index}`);
-
-    now = index + 1;
-
-    if (Number(pizzaNumber.innerHTML) !== now) {
-        pizzaNumber.innerHTML = "0" + now.toFixed();
-        pizzaDots.forEach((item) => {
-            item.setAttribute("class", "");
-        });
-
-        setPizzaDot(index);
-    }
-});
-
-// document.querySelector("#product-show").addEventListener("wheel", (e) => {
-//     console.log(e.deltaY);
-
-//     if (e.deltaY > 0) {
-//         // console.log(`before: scrollLeft: ${pizzaBox.scrollLeft}, single: ${$("#product-show .product-box .product:first").height()}`);
-//         pizzaBox.scrollLeft += pizzaBox.querySelector(".product").clientWidth;
-
-//         if (now !== 5) {
-//             e.preventDefault();
-//         }
-//     } else {
-//         pizzaBox.scrollLeft -= pizzaBox.querySelector(".product").clientWidth;
-//         if (now !== 1) {
-//             e.preventDefault();
-//         }
-//     }
-
-//     console.log(pizzaBox.scrollLeft)
-// });
-
+// 進來的overlay 3秒
 setTimeout(() => {
     $(".loading-animation").fadeOut("slow", () => {
         $("body").removeClass("overflow-hidden");
     });
-}, 3000);
+}, 1000);
 
 document.addEventListener("scroll", () => {
     setNavStatus();
@@ -213,6 +181,7 @@ document.addEventListener("scroll", () => {
         const sectionTop = section.offsetTop;
         if (window.scrollY >= sectionTop - document.querySelector("nav").clientHeight) {
             current = section.getAttribute("id");
+            //console.log("current: ", current);
         }
     });
 
@@ -222,12 +191,144 @@ document.addEventListener("scroll", () => {
             li.classList.add("active");
         }
     });
-
-    // if(current.toLowerCase() === "products"){
-
-    // }
 });
 
+
+// Navigation bar change color
 document.addEventListener("DOMContentLoaded", (target) => {
     setNavStatus();
 });
+function setNavStatus() {
+    if (window.scrollY > 30) {
+        navBar.classList.add("scrolled");
+        hamburgers.forEach((item) => {
+            item.style.background = "#3279ce";
+        });
+    } else {
+        navBar.classList.remove("scrolled");
+        hamburgers.forEach((item) => {
+            item.style.background = "#FFF";
+        });
+    }
+}
+
+// horizontal slider
+!(function($) {
+
+    'use strict';
+  
+    var $slider = $('.scroll-slider'),
+        $slides = $('.scroll-slide'),
+        $sliderWrapper = $('.scroll-wrapper'),
+        $firstSlide = $slides.first();
+
+    var settings = {},
+        resizing = false,
+        scrollController = null,
+        scrollTween = null,
+        scrollTimeline = null,
+        progress = 0,
+        scrollScene = null;
+
+    function scrollSlider(options) {
+
+        // Default
+        settings = $.extend({
+            slider: '.scroll-slider',
+            sliderWrapper: '.scroll-wrapper',
+            slides: '.scroll-slide',
+            slideWidth: null,
+            slideHeight: null,
+        }, options);
+
+        // Set dimensions
+        setDimensions();
+        
+        // On resize        
+        $(window).on( 'resize', function() {
+          clearTimeout(resizing);
+          resizing = setTimeout(function() {
+            setDimensions();
+          }, 250); 
+        });
+      
+    }
+
+    function setDimensions() {
+
+        settings.slideWidth = $firstSlide.width();
+        settings.slideHeight = $firstSlide.height();
+      
+        console.log("slideWidth:", settings.slideWidth);
+        console.log("slideHeight:", settings.slideHeight);
+
+        // Calculate slider width and height
+        console.log("length =", $slides.length);
+        settings.sliderWidth = Math.ceil((settings.slideWidth * $slides.length));
+        settings.sliderHeight = $firstSlide.outerHeight(true);
+
+        console.log("settings.sliderWidth =", settings.sliderWidth);
+
+        // Set slider width and height
+        $sliderWrapper.width(settings.sliderWidth);
+
+        // Set scene
+        setScene();
+
+        //resizing = false;
+    }
+
+    function setScene() {
+
+        var xDist = -$slides.width() * ( $slides.length - 1 ),
+            tlParams = { x: xDist, ease: Power2.easeInOut };
+              
+        if (scrollScene != null && scrollTimeline != null) {
+          
+            progress = 0;
+            scrollScene.progress(progress);
+
+            scrollTimeline = new TimelineMax();
+            scrollTimeline.to( $sliderWrapper, 2, tlParams );
+        
+            scrollScene.setTween(scrollTimeline);
+        
+            scrollScene.refresh();
+            console.log("reset");
+        } else {
+            // Init ScrollMagic controller
+            scrollController = new ScrollMagic.Controller();
+
+            //Create Tween
+            scrollTimeline = new TimelineMax();
+            scrollTimeline.to( $sliderWrapper, 2, tlParams );
+            scrollTimeline.progress( progress );
+
+            // Create scene to pin and link animation
+            scrollScene = new ScrollMagic.Scene({
+                triggerElement: settings.slider,
+                triggerHook: "onLeave",
+                duration: settings.sliderWidth
+            })
+            .setPin(settings.slider)
+            .setTween(scrollTimeline)
+            //.setTween(tl)
+            .addIndicators({
+                colorTrigger: "black",
+                colorStart: "black",
+                colorEnd: "black",
+              })
+            .addTo(scrollController)
+            
+            .on('start', function (event) {
+                scrollTimeline.time(0);
+            });
+        }
+        
+    }
+    
+    $(document).ready(function() {
+        scrollSlider(); 
+    });
+    
+})(jQuery);
